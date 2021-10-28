3d479542730b47bdc6fd5466d75dacbf298989c5
unknown <selbiger10@gmail.com>
2021-10-28 19:19:08 +0300

    Configure ALB for the webservers

--- Homework2/elb.tf
+++ Homework2/elb.tf
@@ -1,24 +1,49 @@
-resource "aws_elb" "web-elb" {
-  name = "web-elb"
-  instances = aws_instance.webservers.*.id
+resource "aws_alb" "webservers" {
+  name = "web-alb"
+  internal = false
+  load_balancer_type = "application"
   security_groups = [aws_security_group.inbound-http-any.id, aws_security_group.outbound-any.id]
   subnets = aws_subnet.public-subnet.*.id
-
-  listener {
-    instance_port = 80
-    instance_protocol = "http"
-    lb_port = 80
-    lb_protocol = "http"
+  access_logs {
+    bucket = data.aws_s3_bucket.main_bucket.bucket
+    prefix = "logs/webservers-alb"
+    enabled = true
   }
+}
 
+resource "aws_alb_listener" "webservers" {
+  load_balancer_arn = aws_alb.webservers.arn
+  port = "80"
+  protocol = "HTTP"
+  default_action {
+    type = "forward"
+    target_group_arn = aws_alb_target_group.webservers.arn
+  }
+}
+resource "aws_alb_target_group" "webservers" {
+  name = "alb-target-group"
+  port = 80
+  protocol = "HTTP"
+  vpc_id = aws_vpc.Whiskey-VPC.id
+  stickiness {
+    type = "lb_cookie"
+    cookie_duration = 60
+    enabled = true
+  }
   health_check {
+    port = 80
+    protocol = "HTTP"
+    path = "/"
     healthy_threshold = 2
     unhealthy_threshold = 2
     timeout = 3
-    target = "HTTP:80/"
     interval = 10
   }
-  tags = {
-    "Name" = "web-elb"
-  }
 }
+
+resource "aws_alb_target_group_attachment" "webservers" {
+  count = length(aws_instance.webservers)
+  target_group_arn = aws_alb_target_group.webservers.arn
+  target_id = aws_instance.webservers.*.id[count.index]
+  port = 80
+  }
