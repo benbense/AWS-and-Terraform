1a5719473d38db3a339a5dbb3d00f837c0126a1e
unknown <selbiger10@gmail.com>
2021-11-14 20:57:54 +0200

    Splitted to folders

---
+++ Homework4/EC2/variables.tf
@@ -0,0 +1,29 @@
+# variable "vpc_name" {
+#   description = "VPC Name"
+#   default     = "Test_VPC"
+# }
+
+# variable "cidr_size" {
+#   description = "CIDR Size"
+#   default     = "10.0.0.0/16"
+# }
+
+# variable "availability_zones" {
+#   description = "Availability zones to account for"
+#   default     = 2
+# }
+
+# variable "instances_to_create" {
+#   description = "EC2 instances to create"
+#   default     = 2
+# }
+
+# variable "instance_type" {
+#   description = "EC2 instances type"
+#   default     = "t2.micro"
+# }
+
+# variable "bucket_name" {
+#   description = "Bucket name for ALB logs"
+#   default     = "ben-opsschool"
+# }
---
+++ Homework4/VPC/provider.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "remote" {
+    organization = "opsschool-ben"
+    workspaces {
+      name = "AWS-and-Terraform"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.63"
+    }
+  }
+}
