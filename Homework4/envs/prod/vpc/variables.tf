4d3dcae34e6c34a6b4e4a118108d513dc840ee99
unknown <selbiger10@gmail.com>
2021-11-14 19:39:59 +0200

    No need for var file?

--- Homework4/envs/prod/vpc/variables.tf
+++ Homework4/envs/prod/vpc/variables.tf
@@ -1,29 +1,29 @@
-variable "vpc_name" {
-  description = "VPC Name"
-  default     = "Test_VPC"
-}
+# variable "vpc_name" {
+#   description = "VPC Name"
+#   default     = "Test_VPC"
+# }
 
-variable "cidr_size" {
-  description = "CIDR Size"
-  default     = "10.0.0.0/16"
-}
+# variable "cidr_size" {
+#   description = "CIDR Size"
+#   default     = "10.0.0.0/16"
+# }
 
-variable "availability_zones" {
-  description = "Availability zones to account for"
-  default     = 2
-}
+# variable "availability_zones" {
+#   description = "Availability zones to account for"
+#   default     = 2
+# }
 
-variable "instances_to_create" {
-  description = "EC2 instances to create"
-  default     = 2
-}
+# variable "instances_to_create" {
+#   description = "EC2 instances to create"
+#   default     = 2
+# }
 
-variable "instance_type" {
-  description = "EC2 instances type"
-  default     = "t2.micro"
-}
+# variable "instance_type" {
+#   description = "EC2 instances type"
+#   default     = "t2.micro"
+# }
 
-variable "bucket_name" {
-  description = "Bucket name for ALB logs"
-  default     = "ben-opsschool"
-}
+# variable "bucket_name" {
+#   description = "Bucket name for ALB logs"
+#   default     = "ben-opsschool"
+# }
