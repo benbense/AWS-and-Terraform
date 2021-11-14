d5bc5ea2fbddf0a3db9fce4611fa47403f353854
unknown <selbiger10@gmail.com>
2021-11-14 20:39:51 +0200

    Returned some of the var pointers

--- Homework4/main.tf
+++ Homework4/main.tf
@@ -9,12 +9,12 @@ module "vpc" {
 
 module "ec2" {
   #source               = "..\\..\\..\\modules\\ec2\\"
-  source  = "benbense/ec2/aws"
-  version = "1.0.0"
-  # vpc_id               = module.vpc.vpc_id
-  # available_zone_names = module.vpc.available_zone_names
-  # public_subnets_ids   = module.vpc.public_subnets_ids
-  # private_subnets_ids  = module.vpc.private_subnets_ids
+  source               = "benbense/ec2/aws"
+  version              = "1.0.0"
+  vpc_id               = module.vpc.vpc_id
+  available_zone_names = module.vpc.available_zone_names
+  public_subnets_ids   = module.vpc.public_subnets_ids
+  private_subnets_ids  = module.vpc.private_subnets_ids
   # instances_to_create  = var.instances_to_create
   # instance_type        = var.instance_type
   # bucket_name          = var.bucket_name
