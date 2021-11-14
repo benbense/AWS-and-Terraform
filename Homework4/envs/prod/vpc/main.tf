7315de32fd2981327ea22a3f9deac1317100b4fc
unknown <selbiger10@gmail.com>
2021-11-14 19:32:49 +0200

    Using the EC2 module on Terraform Cloud Registry

--- Homework4/envs/prod/vpc/main.tf
+++ Homework4/envs/prod/vpc/main.tf
@@ -5,12 +5,7 @@ module "vpc" {
 }
 
 module "ec2" {
-  source               = "..\\..\\..\\modules\\ec2\\"
-  vpc_id               = module.vpc.vpc_id
-  available_zone_names = module.vpc.available_zone_names
-  public_subnets_ids   = module.vpc.public_subnets_ids
-  private_subnets_ids  = module.vpc.private_subnets_ids
-  instances_to_create  = var.instances_to_create
-  instance_type        = var.instance_type
-  bucket_name          = var.bucket_name
+  source  = "benbense/ec2/aws"
+  version = "1.0.0"
+  # Variables are given by the workspace's variables input
 }
