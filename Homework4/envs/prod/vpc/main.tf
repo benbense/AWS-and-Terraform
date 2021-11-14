eaca24bfcd30a49bb3d508339a0ea02d7674aa04
unknown <selbiger10@gmail.com>
2021-11-14 19:42:19 +0200

    Fixed variable issues?

--- Homework4/envs/prod/vpc/main.tf
+++ Homework4/envs/prod/vpc/main.tf
@@ -1,11 +1,21 @@
 module "vpc" {
-  source  = "benbense/vpc/aws"
-  version = "1.0.0"
-  # Variables are given by the workspace's variables input
+  #source             = "..\\..\\..\\modules\\vpc\\"
+  source             = "benbense/ec2/aws"
+  version            = "1.0.0"
+  vpc_name           = var.vpc_name
+  cidr_size          = var.cidr_size
+  availability_zones = 2
 }
 
 module "ec2" {
-  source  = "benbense/ec2/aws"
-  version = "1.0.0"
-  # Variables are given by the workspace's variables input
+  #source               = "..\\..\\..\\modules\\ec2\\"
+  source               = "benbense/ec2/aws"
+  version              = "1.0.0"
+  vpc_id               = module.vpc.vpc_id
+  available_zone_names = module.vpc.available_zone_names
+  public_subnets_ids   = module.vpc.public_subnets_ids
+  private_subnets_ids  = module.vpc.private_subnets_ids
+  instances_to_create  = var.instances_to_create
+  instance_type        = var.instance_type
+  bucket_name          = var.bucket_name
 }
