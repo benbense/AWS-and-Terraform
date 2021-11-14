170e2a2d92120513461578c85c668fcd7d766138
unknown <selbiger10@gmail.com>
2021-11-14 20:38:06 +0200

    Removed vars pointers

--- Homework4/main.tf
+++ Homework4/main.tf
@@ -1,24 +1,21 @@
-provider "aws" {
-  region = var.region
-}
 module "vpc" {
   #source             = "..\\..\\..\\modules\\vpc\\"
-  source             = "benbense/ec2/aws"
-  version            = "1.0.0"
-  vpc_name           = var.vpc_name
-  cidr_size          = var.cidr_size
-  availability_zones = var.availability_zones
+  source  = "benbense/ec2/aws"
+  version = "1.0.0"
+  # vpc_name           = var.vpc_name
+  # cidr_size          = var.cidr_size
+  # availability_zones = var.availability_zones
 }
 
 module "ec2" {
   #source               = "..\\..\\..\\modules\\ec2\\"
-  source               = "benbense/ec2/aws"
-  version              = "1.0.0"
-  vpc_id               = module.vpc.vpc_id
-  available_zone_names = module.vpc.available_zone_names
-  public_subnets_ids   = module.vpc.public_subnets_ids
-  private_subnets_ids  = module.vpc.private_subnets_ids
-  instances_to_create  = var.instances_to_create
-  instance_type        = var.instance_type
-  bucket_name          = var.bucket_name
+  source  = "benbense/ec2/aws"
+  version = "1.0.0"
+  # vpc_id               = module.vpc.vpc_id
+  # available_zone_names = module.vpc.available_zone_names
+  # public_subnets_ids   = module.vpc.public_subnets_ids
+  # private_subnets_ids  = module.vpc.private_subnets_ids
+  # instances_to_create  = var.instances_to_create
+  # instance_type        = var.instance_type
+  # bucket_name          = var.bucket_name
 }
--- Homework4/provider.tf
+++ Homework4/provider.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = var.region
+}
+
 terraform {
   backend "remote" {
     organization = "opsschool-ben"
