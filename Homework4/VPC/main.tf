3bc75fdffac6a0f6002a11917ff25436c8c74483
unknown <selbiger10@gmail.com>
2021-11-21 13:42:35 +0200

    Rmoved reference for EC2 deployment in VPC main.tf

--- Homework4/VPC/main.tf
+++ Homework4/VPC/main.tf
@@ -6,16 +6,3 @@ module "vpc" {
   cidr_size          = var.cidr_size
   availability_zones = var.availability_zones
 }
-
-# module "ec2" {
-#   #source               = "..\\..\\..\\modules\\ec2\\"
-#   source               = "benbense/ec2/aws"
-#   version              = "1.0.0"
-#   vpc_id               = module.vpc.vpc_id
-#   available_zone_names = module.vpc.available_zone_names
-#   public_subnets_ids   = module.vpc.public_subnets_ids
-#   private_subnets_ids  = module.vpc.private_subnets_ids
-#   # instances_to_create  = var.instances_to_create
-#   # instance_type        = var.instance_type
-#   # bucket_name          = var.bucket_name
-# }
