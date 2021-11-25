349f937dc6838ede45b6ca44170952baef92eb0f
unknown <selbiger10@gmail.com>
2021-11-25 12:18:52 +0200

    Moved Github OAuth to provider.tf

--- Homework4/TFC/provider.tf
+++ Homework4/TFC/provider.tf
@@ -10,3 +10,11 @@ terraform {
 provider "tfe" {
   token = var.tfe_token
 }
+
+resource "tfe_oauth_client" "github_oauth" {
+  organization     = var.tfe_organization_name
+  api_url          = "https://api.github.com"
+  http_url         = "https://github.com"
+  oauth_token      = var.github_pat
+  service_provider = "github"
+}
